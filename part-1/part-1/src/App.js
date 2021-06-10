4d6f964491d5d674c2d091bf6e9bcc69219cc7c8
pradeep7G <pradeep7chinna@gmail.com>
2021-06-10 21:52:04 +0530

    snapshot of practice upto part-1 d (50%)

--- part-1/part-1/src/App.js
+++ part-1/part-1/src/App.js
@@ -61,36 +61,99 @@ import React,{useState} from 'react';
 //   )
 // }
 
-const Display=({counter})=>{
+// const Display=({counter})=>{
+//   return (
+//     <div>
+//       {counter}
+//     </div>
+//   )
+// }
+
+// const Button=({handleClick,text})=>{
+//   return (
+//    <button onClick={handleClick}>
+//      {text}
+//    </button>
+//   )
+// }
+
+// const App=(props)=>{
+//   const [counter,setCounter]=useState(0);
+//   const increaseByOne=()=>setCounter(counter+1);
+//   const decreaseByOne=()=>setCounter(counter-1);
+//   const setToZero=()=>setCounter(0);
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button handleClick={increaseByOne} text='plus' />
+//       <Button handleClick={decreaseByOne} text='minus' />
+//       <Button handleClick={setToZero} text='zero' />
+//     </div>
+//   )
+// }
+const History=(props)=>{
+  if(props.allClicks.length === 0)
+  {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      {counter}
+      button press History : {props.allClicks.join(' ')}
     </div>
   )
 }
 
 const Button=({handleClick,text})=>{
   return (
-   <button onClick={handleClick}>
-     {text}
-   </button>
+  <button onClick={handleClick}>
+    {text}
+  </button>
   )
 }
+const App=()=>{
+  const [left,setLeft]=useState(0);
+  const [right,setRight]=useState(0);
+  const [allClicks,setAll]=useState([]);
+  // const [clicks,setClicks]=useState({
+  //   left:0,
+  //   right:0
+  // });
+  // const handleLeftClick=()=>{
+  //   // const newClick={
+  //   //  ...clicks,
+  //   //  left:clicks.left+1
+  //   // }
+  //   setClicks({...clicks,left:clicks.left+1});
+  // }
 
-const App=(props)=>{
-  const [counter,setCounter]=useState(0);
-  const increaseByOne=()=>setCounter(counter+1);
-  const decreaseByOne=()=>setCounter(counter-1);
-  const setToZero=()=>setCounter(0);
+  // const handleRightClick=()=>{
+  //   // const newClick={
+  //   //   ...clicks,
+  //   //   right:clicks.right+1
+  //   // }
+  //   setClicks({...clicks,right:clicks.right+1});
+  // }
+  const handleLeftClick=()=>{
+    setAll(allClicks.concat('L'));
+    setLeft(left+1);
+  }
+  const handleRightClick=()=>{
+    setAll(allClicks.concat('R'));
+    setRight(right+1);
+  }
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text='plus' />
-      <Button handleClick={decreaseByOne} text='minus' />
-      <Button handleClick={setToZero} text='zero' />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
-
 
 export default App;
