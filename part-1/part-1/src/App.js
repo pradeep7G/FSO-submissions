8ea15451ce8f2b083482f3ff49f8a8441b21d1e8
pradeep7G <pradeep7chinna@gmail.com>
2021-06-11 16:06:38 +0530

    snapshots of practise in part-1 and add part-2 directory

--- part-1/part-1/src/App.js
+++ part-1/part-1/src/App.js
@@ -1,74 +1,48 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Button=({handleClick,text})=>{
+const Content=({parts})=>{
   return (
-  <button onClick={handleClick}>{text}</button>
+    <div>
+   {
+   parts.map((part)=><div key={part.id}>{part.name} {part.exercises}</div>)
+   }
+   </div>
   )
 }
 
-const DisplayAnecdote=({anecdote,vote})=>{
-
-  return (
+const Course =({course})=>{
+  return(
     <div>
-      {anecdote} <br/>
-      has {vote} votes <br/>
+    <h1>{course.name}</h1>
+    <Content key={course.id} parts={course.parts} />
     </div>
-   )
+  )
 }
 
 const App = () => {
-
-  const [selected, setSelected] = useState(0);
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
-  ]
-  const obj={};
-  for(let i=0;i<anecdotes.length;i++)
-  {
-    obj[i]=0;
-  }
-  const [votes, setVotes] = useState(obj);
-  
-  const selectRandom=()=>{
-     const num=Math.random()*(anecdotes.length);
-     setSelected(Math.floor(num));
-  }
-  let Max=0,key=0;
-  const vote=()=>{
-    const newObj={...votes};
-    newObj[selected]+=1;
-    setVotes(newObj);
-  }
-  
-   for(let j=0;j<anecdotes.length;j++)
-    {
-      if(Max < votes[j])
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
       {
-        Max=votes[j];
-        key=j;
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
       }
-    }
+    ]
+  }
 
-  return (
-    <>
-    <div>
-      <h1>Anecdote of the day</h1>
-      <DisplayAnecdote anecdote={anecdotes[selected]} vote={votes[selected]} />
-      <Button handleClick={vote} text='vote' />
-      <Button handleClick={selectRandom} text='next anecdote' />
-      </div>
-      <div>
-      <h1>Anecdote with most votes</h1>
-      <DisplayAnecdote anecdote={anecdotes[key]} vote={Max} />
-    </div>
-    </>
-  )
+  return <Course course={course} />
 }
 
 export default App
