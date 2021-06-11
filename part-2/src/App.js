8e76b6095972be73a73b35f7c11be970693cd783
pradeep7G <pradeep7chinna@gmail.com>
2021-06-11 21:18:28 +0530

    completed ex-2.10

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -6,6 +6,33 @@ const DisplayPerson=({name,number})=>{
   )
 }
 
+const Filter=({search,handleSearch,person})=>{
+  return (
+    <>
+    <div><strong>filter shown with : </strong> <input value={search} onChange={handleSearch} /></div>
+    <Search search={search} person={person} />
+   </>
+  )
+}
+
+const PersonForm=({addPerson,handleOnChange,newNumber,handleNumberInfo,newName})=>{
+  return (
+   <form onSubmit={addPerson}>
+        <div>name:<input value={newName} onChange={handleOnChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberInfo}/></div>
+        <div><button type="submit">add</button></div>
+   </form>
+  )
+}
+
+const Persons=({person})=>{
+    return (
+      <div>
+        {person.map((personn)=> <DisplayPerson key={personn.name} name={personn.name} number={personn.number} />)}
+      </div>
+    )
+}
+
 const Search=({search,person})=>{
   let ar=[];
   person.forEach((p)=>{
@@ -14,16 +41,10 @@ const Search=({search,person})=>{
       ar.push(p);
     }
   })
-  console.log(ar);
+
   return (
     <div>
-    {
-      ar.map((p)=>{
-        return (
-        <div key={p.name}>{p.name} {p.number}</div>
-        )
-      })
-    }
+    {ar.map((p)=><div key={p.name}>{p.name} {p.number}</div>)}
     </div>
   )
 }
@@ -81,25 +102,20 @@ const App=()=>{
   
   return (
     <div>
-      <h1>Phonebook</h1>
-        <div>filter shown with : <input value={search} onChange={handleSearch} /></div>
-        <Search search={search} person={person} />
-      <h2>Add a new </h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:<input value={newName} onChange={handleOnChange}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberInfo}/></div>
-        <div>
-          <button type="submit">
-            add
-          </button>
-        </div>
-      </form>
+      <h2>Phonebook</h2>
+      <Filter search={search} handleSearch={handleSearch} person={person} />
+
+      <h3>Add a new </h3>
+      <PersonForm 
+      addPerson={addPerson}
+      handleOnChange={handleOnChange}
+      newNumber={newNumber}
+      handleNumberInfo={handleNumberInfo}
+      newName={newName}
+      />
+
       <h2>Numbers</h2>
-      <div>
-      {person.map((personn)=> <DisplayPerson key={personn.name} name={personn.name} number={personn.number} />)}
-      </div>
+      <Persons person={person} />
     </div>
   )
 }
