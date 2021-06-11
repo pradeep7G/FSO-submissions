1536d7400d7a594cffee5655968240605f49a0c8
pradeep7G <pradeep7chinna@gmail.com>
2021-06-11 17:05:12 +0530

    completed exercise 2.5 created a module for Course

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -1,48 +1,5 @@
 import React from 'react';
-
-const Header=(props)=>{
-  return (
-   <div>
-     <h2>{props.course}</h2>
-   </div>
-  )
-}
-
-const Part=({part})=>{
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-const Content=({parts})=>{
-  return (
-  <div>
-    {parts.map((part)=><Part key={part.id} part={part}/>)}
-  </div>
-  )
-}
-const Total=({parts})=>{
-
-  const total=parts.reduce((acc,cur)=> acc + cur.exercises,0);
-  return (
-    <>
-      <p><b>Total of {total} exercises</b></p>
-    </>
-  )
-}
-
-const Course=({course})=>{
-
-      return (
-        <div>
-        <Header course={course.name} />
-        <Content parts={course.parts}  />
-        <Total parts={course.parts}/>
-        </div>
-      )
-}
-
+import Course from './components/Course';
 const App = () => {
 
   const courses = [
