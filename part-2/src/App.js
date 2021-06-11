09103f40fa30387b119c50e9746103cf5c4cef1f
pradeep7G <pradeep7chinna@gmail.com>
2021-06-11 19:22:23 +0530

    fixed some logic in ex-2.7 and ex-2.8 done

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -2,35 +2,53 @@ import React,{useState} from 'react';
 
 const App=()=>{
   const [person,setPerson]=useState([
-    {name:'Pradeep P'},
+    {name:'Pradeep P',number:"7989266"},
   ])
   
   const [newName,setNewName]=useState('');
+  const [newNumber,setNewNumber]=useState('');
 
-  const addName=(event)=>{
+  const addPerson=(event)=>{
     event.preventDefault();
-    const newObj={name:newName};
-    setPerson(person.concat(newObj));
-    setNewName('');
+    const newObj={name:newName,number:newNumber};
+     let found=person.find(element => element.name === newName);
+       if(found && found.name===newName)
+       {
+          alert(`${newName} is already added to phonebook`)
+       }
+       else
+       {
+          found=person.find(element => element.number === newNumber);
+          if(found && found.number===newNumber)
+          {
+              alert(`${newNumber} is already added to phonebook`)
+          }
+          else
+          {
+              setPerson(person.concat(newObj));
+              setNewName('');
+              setNewNumber('');
+          }
+      }
   }
 
   const handleOnChange=(event)=>{
       const newPerson=event.target.value;
-       const found=person.find(element => element.name === newPerson);
-       if(found && found.name===newPerson)
-       {
-         alert(`${newPerson} is already added to phonebook`)
-       }
       setNewName(newPerson);
   }
 
+  const handleNumberInfo=(event)=>{
+    const newPNumber=event.target.value;
+      setNewNumber(newPNumber);
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name:<input value={newName} onChange={handleOnChange}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberInfo}/></div>
         <div>
           <button type="submit">
             add
@@ -39,7 +57,7 @@ const App=()=>{
       </form>
       <h2>Numbers</h2>
       <div>
-      {person.map((personn)=><div key={personn.name}>{personn.name}</div>)}
+      {person.map((personn)=><div key={personn.name}>{personn.name} - {personn.number}</div>)}
       </div>
     </div>
   )
